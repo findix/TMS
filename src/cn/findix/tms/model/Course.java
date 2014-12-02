41e6f65e4d89608f5cf9244948a0690f25f3d90b
long <493377745@qq.com>
2014-12-02 16:06:57 +0800

    course_info页面显示、course_info和teacher_info数据表格
    Signed-off-by: long <493377745@qq.com>

--- src/cn/findix/tms/model/Course.java
+++ src/cn/findix/tms/model/Course.java
@@ -2,9 +2,23 @@ package cn.findix.tms.model;
 
 import com.jfinal.plugin.activerecord.Model;
 
+import java.util.List;
+
 /**
  * Created by Sean on 2014/11/28.
  */
 public class Course extends Model<Course> {
     public static final Course DAO = new Course();
+
+    public String getDepartmentName() {
+        return Department.DAO.findById(get("did")).get("dname");
+    }
+
+    public String getMajorName() {
+        return Major.DAO.findById(get("mid")).get("mname");
+    }
+
+    public List<Course> findAll(){
+        return find("SELECT course.cid, course.cname, course.cenglish, course.credit, course.did, department.dname, course.mid,major.mname FROM course, department, major WHERE course.did = department.did AND course.mid=major.mid ORDER BY course.cid ASC");
+    }
 }
