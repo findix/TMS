ea497df0d1272dfce6f9d7bed3da0ca7e4f6a1e1
Sean Fung <fengxiang220@gmail.com>
2014-12-02 15:14:29 +0800

    完成教师管理增删改查
    Signed-off-by: Sean Fung <fengxiang220@gmail.com>

--- src/cn/findix/tms/controller/TeacherController.java
+++ src/cn/findix/tms/controller/TeacherController.java
@@ -9,11 +9,29 @@ import com.jfinal.core.Controller;
 public class TeacherController extends Controller {
 
     public void info() {
-        setAttr("teachers",Teacher.DAO.findAll());
+        setAttr("teachers", Teacher.DAO.findAll());
         render("teacher_info.jsp");
     }
 
     public void edit() {
         render("teacher_edit.jsp");
+    }
+
+    /**
+     * Ajax API to save teacher info
+     */
+    public void saveAndAdd() {
+        if (Teacher.DAO.isExisted(getPara("teacher.tid"))) {
+            getModel(Teacher.class).update();
+            renderText("SUCCESS");
+        } else {
+            getModel(Teacher.class).set("password", "123456").save();
+            renderText("SUCCESS");
+        }
+    }
+
+    public void delete() {
+        getModel(Teacher.class).delete();
+        renderText("SUCCESS");
     }
 }
--- src/cn/findix/tms/model/Teacher.java
+++ src/cn/findix/tms/model/Teacher.java
@@ -19,6 +19,11 @@ public class Teacher extends Model<Teacher> {
         return DAO.findFirst(sql, id, password) != null;
     }
 
+    public boolean isExisted(String id) {
+        String sql = "select tname from Teacher where tid=?";
+        return DAO.findFirst(sql, id) != null;
+    }
+
     public List<Teacher> findAll() {
         return find("SELECT teacher.tid, teacher.tname, teacher.`password`, teacher.type, teacher.did, department.dname, auth.aname FROM teacher, department, auth WHERE teacher.did = department.did AND teacher.type = auth.type ORDER BY teacher.tid ASC");
     }
